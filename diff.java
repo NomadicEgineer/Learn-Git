e15309260d09575e44e1f2e53ad5a8c2082c8b6a
Nomadic Engineer <darshanhande12345@gmail.com>
2025-11-26 15:56:02 +0530

    updated sum and diff file

--- diff.java
+++ diff.java
@@ -1,3 +1,8 @@
 class diff{
-  
+    public static void main(String[] args){
+      int a=5;
+      int b=10;
+      int c=b-a;
+      System.out.println("Diff of a and b "+ c );
+  }
 }
--- sum.java
+++ sum.java
@@ -2,6 +2,7 @@ class sum{
   public static void main(String[] args){
       int a=5;
       int b=10;
-      System.out.println("Sum of a and b "+(a+b));
+      int c=a+b;
+      System.out.println("Sum of a and b "+ c );
   }
 }
